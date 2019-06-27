7bb22f8de9b2418bb3eb21aa9d71cb30fd69c0b3
luisandia <luisandiamamani@gmail.com>
2019-06-27 01:50:49 +0000

    add env, mongodb connection

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("DB UP!");
+}).catch((e) => {
+    console.error(e);
+});
 
 const server = new ApolloServer({
     typeDefs,
