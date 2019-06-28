cbc1b021f8734b0254adb1547de65da5eb6adf37
luisandia <luisandiamamani@gmail.com>
2019-06-28 00:17:28 +0000

    add gogole auth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log(idToken)
+  }
+
+  return <GoogleLogin
+    clientId="1076905569446-nedm2j9cg8k4gf5g8ega71pscnlt2o23.apps.googleusercontent.com"
+    onSuccess={onSuccess}
+    isSignedIn={true} />
 };
 
 const styles = {
